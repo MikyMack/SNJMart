bfa0c6cf792adf746c6e12e472b13f03b413e93f
MikyMack <jijomonjs@gmail.com>
2023-07-14 13:08:27 +0530

    update

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,31 +1,26 @@
 const multer=require('multer');
 const path =require('path');
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-      cb(null, path.resolve(__dirname, "../public/productImages"))
-  },
-  filename: (req, file, cb) => {
-      const name = `${Date.now()}-${file.originalname}`
-      cb(null, name)
-  }
-})
-const upload = multer({ storage: storage })
-
-const categoryStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-      cb(null, path.resolve(__dirname, "../public/categoryImages"))
-  },
-  filename: (req, file, cb) => {
-      const name = `${Date.now()}-${file.originalname}`
-      cb(null, name)
-  }
-})
-const categoryUpload = multer({ storage: categoryStorage })
+let Storage = multer.diskStorage({
+    destination: "./public/uploads/",
+    filename: (req, file, cb) => {
+      cb(
+        null,
+        file.fieldname + "_" + Date.now() + path.extname(file.originalname)
+      );
+    },
+  });
+  let upload = multer({
+    storage: Storage,fileFilter: function (params, file, callback) {
+        if (file.mimetype == "image/png" || file.mimetype == "image/jpeg" || file.mimetype == "image/jpg") {
+          callback(null, true)
+        } else {
+          console.log('only jpg & png file supported !');
+          callback(null, false)
+ }
+}
+  })
   
 module.exports={
     upload,
-    storage,
-    categoryUpload
-
 }
